1ec4fa0d60a2e0e61569f8e91fc54addf86160a7
Filipe Furtado <filipegomesfurtado@gmail.com>
2018-10-10 21:41:41 -0300

    Update models.py

--- lms/lms_app/models.py
+++ lms/lms_app/models.py
@@ -58,13 +58,13 @@ class DisciplinaOfertada(models.Model):
 
             turma_dobrado = DisciplinaOfertada.objects.filter(turma = self.turma)
             if len(turma_dobrado)>0:
-                ano_dobrado = DisciplinaOfertada.objects.filter(curso = self.ano)
+                ano_dobrado = DisciplinaOfertada.objects.filter(ano = self.ano)
                 if len(ano_dobrado)>0:
                     semestre_dobrado = DisciplinaOfertada.objects.filter(semestre = self.semestre)
                     if len(semestre_dobrado)>0:
                         professor_dobrado = DisciplinaOfertada.objects.filter(professor = self.professor)
                         if len(professor_dobrado)>0:
-                            disciplina_dobrado = DisciplinaOfertada.objects.filter(curso = self.disciplina)
+                            disciplina_dobrado = DisciplinaOfertada.objects.filter(disciplina = self.disciplina)
                             if len(disciplina_dobrado)>0:
                                  raise Exception('disciplina dobrada')    
 
